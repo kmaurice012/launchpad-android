3455a81f11f0313b9f47923089c3eb0b69362208
King Maurice <90697810+kmaurice012@users.noreply.github.com>
2023-01-26 17:29:05 +0300

    Initial commit

---
+++ app/src/main/java/com/amlwin/android/launchpad/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.amlwin.android.launchpad
+
+import android.app.Application
+
+/**
+ * a touch of amlwin on 08/04/2020.
+ * made with <3
+ */
+
+class MainApplication : Application() {
+}
